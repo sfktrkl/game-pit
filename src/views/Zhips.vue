53f1b717dea4e96625032bde1d73a5938329052a
Safak TURKELI <sfktrkl@gmail.com>
2020-10-30 18:44:34 +0300

    Generate enemy ships and make them hitable, Zhips
    * Placed ships are drawn and during the placement placing ships on top of each other is avoided.
    * After placement stage is finished, enemy ships are generated.
    * During the attacking stage every tile hit is drawn according to an enemy ship is hit or not.

--- src/views/Zhips.vue
+++ src/views/Zhips.vue
@@ -3,7 +3,7 @@
     <div id="name">Zhips</div>
     <div id="instructions">Under construction :)</div>
     <canvas id="sea" tabindex="0" @mousemove="update_sea($event)"
-      @mousedown="place_ship($event)">
+      @mousedown="place_attack_ship()">
     </canvas>
   </div>
 </template>
@@ -26,6 +26,11 @@ export default {
 
       // Locations of ships
       ship_locations: [],
+      enemy_ship_locations: [],
+
+      // Hit tiles
+      hit_tiles: [],
+      enemy_hit_tiles: [],
 
       // Color multipliers
       color_default: [1, 1, 1],
@@ -155,11 +160,53 @@ export default {
         }
       }
       this.change_sea_color(color, dx, width, dy, height);
+
+      // Draw placed ships
+      var ship_tiles = this.get_ship_tiles(this.ship_locations);
+      if (ship_tiles.length > 0) {
+        ship_tiles.forEach(tiles => {
+            this.change_sea_color(this.color_reddish, tiles[0], 64, tiles[1], 64);
+        })
+      }
+
+      // Draw every hit tile,
+      // green color for already hit empty tiles,
+      // red color for hit ship tiles
+      if (this.enemy_hit_tiles.length > 0) {
+        this.enemy_hit_tiles.forEach(tiles => {
+            this.change_sea_color(tiles[2] == 1 ? this.color_reddish : this.color_greenish, tiles[0], 64, tiles[1], 64);
+        })
+      }
+
+      this.drawing = false;
+    },
+    get_ship_tiles: function(ship_locations) {
+      var ship_tiles = [];
+      if (ship_locations) {
+        ship_locations.forEach(ships => {
+          for (var ship in ships) {
+            ships[ship].forEach(tiles => {
+              ship_tiles.push(tiles);
+            });
+          }
+        });
+      }
+      return ship_tiles;
+    },
+    check_collision: function (existing_tiles, ship_tiles) {
+      // Check whether given ship tile, collides with an existing one
+      // return 1 if it is colliding.
+      var existing_ship_tiles = this.get_ship_tiles(existing_tiles);
+      for (let i = 0; i < existing_ship_tiles.length; i++) {
+        for (let j = 0; j < ship_tiles.length; j++) {
+          if (existing_ship_tiles[i][0] == ship_tiles[j][0] &&
+              existing_ship_tiles[i][1] == ship_tiles[j][1])
+              return 0;
+        }
+      }
+      return 1;
     },
     place_ship: function () {
-      if (this.cannot_place)
-        return;
-
       // Get the current ship and calculate its tiles locations
       var ship_tiles = [];
       var current_ship = this.get_current_ship();
@@ -168,6 +215,9 @@ export default {
           ship_tiles.push([this.m_rounded_x + i * 64, this.m_rounded_y + j * 64]);
         }
       }
+
+      if(!this.check_collision(this.ship_locations, ship_tiles))
+        return;
 
       // Store ship tiles in ship locations array
       var ship_location = {};
@@ -181,10 +231,80 @@ export default {
       // start attacking to enemy.
       if (this.ship_to_place >= this.ships.length)
       {
+        this.generate_enemy_ships();
+
         this.game_state = 'attack';
         this.cannot_place = null;
         this.cannot_attack = false;
       }
+    },
+    attack: function () {
+      var hit = 0;
+      // Check that tile is already hit.
+      for (let i = 0; i < this.enemy_hit_tiles.length; i++)
+      {
+        var enemy_hit_tile = this.enemy_hit_tiles[i];
+        if (this.m_rounded_x == enemy_hit_tile[0] && this.m_rounded_y == enemy_hit_tile[1])
+          return;
+      }
+
+      // Check any ship is hit,
+      // if it is make that tile hit.
+      var enemy_ship_tiles = this.get_ship_tiles(this.enemy_ship_locations);
+      for (let i = 0; i < enemy_ship_tiles.length; i++)
+      {
+        var enemy_ship_tile = enemy_ship_tiles[i];
+        if (this.m_rounded_x == enemy_ship_tile[0] && this.m_rounded_y == enemy_ship_tile[1])
+          hit = 1;
+      }
+
+      this.enemy_hit_tiles.push([this.m_rounded_x, this.m_rounded_y, hit]);
+    },
+    getRandomTile: function (dir) {
+      return Math.floor(Math.random() * Math.floor(dir == "x" ? 9 : 8)) * 64;
+    },
+    generate_enemy_ships: function () {
+      // Get the current ship and calculate its tiles locations
+      for (let i = 0; i < this.ships.length; i++) {
+        var ship_tiles = [];
+
+        var current_ship = this.ships[i];
+        var random_x = this.getRandomTile("x");
+        var random_y = this.getRandomTile("y");
+
+        var width = 64 * current_ship[0];
+        var height = 64 * current_ship[1];
+        // Only allow ships which fits the sea
+        if (random_x + width <= this.sea.width && random_y + height <= this.sea.height)
+        {
+          for (let i = 0; i < current_ship[0]; i++) {
+            for (let j = 0; j < current_ship[1]; j++) {
+              ship_tiles.push([random_x + i * 64, random_y + j * 64]);
+            }
+          }
+        }
+
+        // If no ship tile is generated (random values
+        // does not fit the sea) or generated tiles
+        // collide with existing ships, reiterate.
+        if(ship_tiles.length < 1 ||
+          !this.check_collision(this.enemy_ship_locations, ship_tiles))
+        {
+          i--;
+          continue;
+        }
+
+        // Store ship tiles in ship locations array
+        var ship_location = {};
+        ship_location[this.ship_to_place] = ship_tiles;
+        this.enemy_ship_locations.push(ship_location);
+      }
+    },
+    place_attack_ship: function () {
+      if (this.cannot_place == false)
+        this.place_ship();
+      else if (this.cannot_attack == false)
+        this.attack();
     }
   },
   created() {
@@ -194,6 +314,7 @@ export default {
     img.src = "./assets/zhips/canvas.png";
     img.onload = () => {
       this.canvas_img = img;
+      this.clear_sea();
     };
   },
   mounted() {
