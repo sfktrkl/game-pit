bb991e9c5afa73dd5f43ea029e678c189dc7aafa
Safak TURKELI <sfktrkl@gmail.com>
2022-07-31 23:56:22 +0300

    Make animations for cards in Home page
    * Cards in the home page are arranged to have animations.
    * Images of the cards are updated.

--- src/App.vue
+++ src/App.vue
@@ -1,80 +1,85 @@
 <template>
   <div class="app">
-    <div class="outer">
-      <div class="inner">
+    <div class="app-outer">
+      <div class="app-inner">
         <app-header></app-header>
         <router-view></router-view>
       </div>
 
       <div class="snow">
-        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="99%"
-             viewBox="0 0 1024 490" preserveAspectRatio="none">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="100%"
+          height="99%"
+          viewBox="0 0 1024 490"
+          preserveAspectRatio="none"
+        >
           <g fill="#FFF" fill-opacity=".15" transform="translate(55 42)">
             <g id="snow-bottom-layer">
-              <ellipse cx="6" cy="1009.5" rx="6" ry="5.5"/>
-              <ellipse cx="138" cy="1110.5" rx="6" ry="5.5"/>
-              <ellipse cx="398" cy="1055.5" rx="6" ry="5.5"/>
-              <ellipse cx="719" cy="1284.5" rx="6" ry="5.5"/>
-              <ellipse cx="760" cy="1155.5" rx="6" ry="5.5"/>
-              <ellipse cx="635" cy="1459.5" rx="6" ry="5.5"/>
-              <ellipse cx="478" cy="1335.5" rx="6" ry="5.5"/>
-              <ellipse cx="322" cy="1414.5" rx="6" ry="5.5"/>
-              <ellipse cx="247" cy="1234.5" rx="6" ry="5.5"/>
-              <ellipse cx="154" cy="1425.5" rx="6" ry="5.5"/>
-              <ellipse cx="731" cy="773.5" rx="6" ry="5.5"/>
-              <ellipse cx="599" cy="874.5" rx="6" ry="5.5"/>
-              <ellipse cx="339" cy="819.5" rx="6" ry="5.5"/>
-              <ellipse cx="239" cy="1004.5" rx="6" ry="5.5"/>
-              <ellipse cx="113" cy="863.5" rx="6" ry="5.5"/>
-              <ellipse cx="102" cy="1223.5" rx="6" ry="5.5"/>
-              <ellipse cx="395" cy="1155.5" rx="6" ry="5.5"/>
-              <ellipse cx="826" cy="943.5" rx="6" ry="5.5"/>
-              <ellipse cx="626" cy="1054.5" rx="6" ry="5.5"/>
-              <ellipse cx="887" cy="1366.5" rx="6" ry="5.5"/>
-              <ellipse cx="6" cy="241.5" rx="6" ry="5.5"/>
-              <ellipse cx="138" cy="342.5" rx="6" ry="5.5"/>
-              <ellipse cx="398" cy="287.5" rx="6" ry="5.5"/>
-              <ellipse cx="719" cy="516.5" rx="6" ry="5.5"/>
-              <ellipse cx="760" cy="387.5" rx="6" ry="5.5"/>
-              <ellipse cx="635" cy="691.5" rx="6" ry="5.5"/>
-              <ellipse cx="478" cy="567.5" rx="6" ry="5.5"/>
-              <ellipse cx="322" cy="646.5" rx="6" ry="5.5"/>
-              <ellipse cx="247" cy="466.5" rx="6" ry="5.5"/>
-              <ellipse cx="154" cy="657.5" rx="6" ry="5.5"/>
-              <ellipse cx="731" cy="5.5" rx="6" ry="5.5"/>
-              <ellipse cx="599" cy="106.5" rx="6" ry="5.5"/>
-              <ellipse cx="339" cy="51.5" rx="6" ry="5.5"/>
-              <ellipse cx="239" cy="236.5" rx="6" ry="5.5"/>
-              <ellipse cx="113" cy="95.5" rx="6" ry="5.5"/>
-              <ellipse cx="102" cy="455.5" rx="6" ry="5.5"/>
-              <ellipse cx="395" cy="387.5" rx="6" ry="5.5"/>
-              <ellipse cx="826" cy="175.5" rx="6" ry="5.5"/>
-              <ellipse cx="626" cy="286.5" rx="6" ry="5.5"/>
-              <ellipse cx="887" cy="598.5" rx="6" ry="5.5"/>
+              <ellipse cx="6" cy="1009.5" rx="6" ry="5.5" />
+              <ellipse cx="138" cy="1110.5" rx="6" ry="5.5" />
+              <ellipse cx="398" cy="1055.5" rx="6" ry="5.5" />
+              <ellipse cx="719" cy="1284.5" rx="6" ry="5.5" />
+              <ellipse cx="760" cy="1155.5" rx="6" ry="5.5" />
+              <ellipse cx="635" cy="1459.5" rx="6" ry="5.5" />
+              <ellipse cx="478" cy="1335.5" rx="6" ry="5.5" />
+              <ellipse cx="322" cy="1414.5" rx="6" ry="5.5" />
+              <ellipse cx="247" cy="1234.5" rx="6" ry="5.5" />
+              <ellipse cx="154" cy="1425.5" rx="6" ry="5.5" />
+              <ellipse cx="731" cy="773.5" rx="6" ry="5.5" />
+              <ellipse cx="599" cy="874.5" rx="6" ry="5.5" />
+              <ellipse cx="339" cy="819.5" rx="6" ry="5.5" />
+              <ellipse cx="239" cy="1004.5" rx="6" ry="5.5" />
+              <ellipse cx="113" cy="863.5" rx="6" ry="5.5" />
+              <ellipse cx="102" cy="1223.5" rx="6" ry="5.5" />
+              <ellipse cx="395" cy="1155.5" rx="6" ry="5.5" />
+              <ellipse cx="826" cy="943.5" rx="6" ry="5.5" />
+              <ellipse cx="626" cy="1054.5" rx="6" ry="5.5" />
+              <ellipse cx="887" cy="1366.5" rx="6" ry="5.5" />
+              <ellipse cx="6" cy="241.5" rx="6" ry="5.5" />
+              <ellipse cx="138" cy="342.5" rx="6" ry="5.5" />
+              <ellipse cx="398" cy="287.5" rx="6" ry="5.5" />
+              <ellipse cx="719" cy="516.5" rx="6" ry="5.5" />
+              <ellipse cx="760" cy="387.5" rx="6" ry="5.5" />
+              <ellipse cx="635" cy="691.5" rx="6" ry="5.5" />
+              <ellipse cx="478" cy="567.5" rx="6" ry="5.5" />
+              <ellipse cx="322" cy="646.5" rx="6" ry="5.5" />
+              <ellipse cx="247" cy="466.5" rx="6" ry="5.5" />
+              <ellipse cx="154" cy="657.5" rx="6" ry="5.5" />
+              <ellipse cx="731" cy="5.5" rx="6" ry="5.5" />
+              <ellipse cx="599" cy="106.5" rx="6" ry="5.5" />
+              <ellipse cx="339" cy="51.5" rx="6" ry="5.5" />
+              <ellipse cx="239" cy="236.5" rx="6" ry="5.5" />
+              <ellipse cx="113" cy="95.5" rx="6" ry="5.5" />
+              <ellipse cx="102" cy="455.5" rx="6" ry="5.5" />
+              <ellipse cx="395" cy="387.5" rx="6" ry="5.5" />
+              <ellipse cx="826" cy="175.5" rx="6" ry="5.5" />
+              <ellipse cx="626" cy="286.5" rx="6" ry="5.5" />
+              <ellipse cx="887" cy="598.5" rx="6" ry="5.5" />
             </g>
           </g>
           <g fill="#FFF" fill-opacity=".3" transform="translate(65 63)">
             <g id="snow-top-layer">
-              <circle cx="8" cy="776" r="8"/>
-              <circle cx="189" cy="925" r="8"/>
-              <circle cx="548" cy="844" r="8"/>
-              <circle cx="685" cy="1115" r="8"/>
-              <circle cx="858" cy="909" r="8"/>
-              <circle cx="874" cy="1438" r="8"/>
-              <circle cx="657" cy="1256" r="8"/>
-              <circle cx="443" cy="1372" r="8"/>
-              <circle cx="339" cy="1107" r="8"/>
-              <circle cx="24" cy="1305" r="8"/>
-              <circle cx="8" cy="8" r="8"/>
-              <circle cx="189" cy="157" r="8"/>
-              <circle cx="548" cy="76" r="8"/>
-              <circle cx="685" cy="347" r="8"/>
-              <circle cx="858" cy="141" r="8"/>
-              <circle cx="874" cy="670" r="8"/>
-              <circle cx="657" cy="488" r="8"/>
-              <circle cx="443" cy="604" r="8"/>
-              <circle cx="339" cy="339" r="8"/>
-              <circle cx="24" cy="537" r="8"/>
+              <circle cx="8" cy="776" r="8" />
+              <circle cx="189" cy="925" r="8" />
+              <circle cx="548" cy="844" r="8" />
+              <circle cx="685" cy="1115" r="8" />
+              <circle cx="858" cy="909" r="8" />
+              <circle cx="874" cy="1438" r="8" />
+              <circle cx="657" cy="1256" r="8" />
+              <circle cx="443" cy="1372" r="8" />
+              <circle cx="339" cy="1107" r="8" />
+              <circle cx="24" cy="1305" r="8" />
+              <circle cx="8" cy="8" r="8" />
+              <circle cx="189" cy="157" r="8" />
+              <circle cx="548" cy="76" r="8" />
+              <circle cx="685" cy="347" r="8" />
+              <circle cx="858" cy="141" r="8" />
+              <circle cx="874" cy="670" r="8" />
+              <circle cx="657" cy="488" r="8" />
+              <circle cx="443" cy="604" r="8" />
+              <circle cx="339" cy="339" r="8" />
+              <circle cx="24" cy="537" r="8" />
             </g>
           </g>
         </svg>
@@ -84,18 +89,20 @@
 </template>
 
 <script>
-import Header from './components/Header.vue';
+import Header from "./components/Header.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    'app-header': Header,
-  }
-}
+    "app-header": Header,
+  },
+};
 </script>
 
 <style>
-html, body, #app {
+html,
+body,
+#app {
   margin: 0;
   height: 100%;
 }
@@ -104,13 +111,14 @@ html, body, #app {
   height: 100%;
 }
 
-.outer {
+.app-outer {
   height: 100%;
-  background: radial-gradient(ellipse at top, #008000 40%, #000B80 160%);
+  background: radial-gradient(ellipse at top, #008000 40%, #000b80 160%);
 }
 
-.inner {
+.app-inner {
   width: 100%;
+  height: 100%;
   position: absolute;
 }
 
@@ -133,8 +141,8 @@ html, body, #app {
 }
 
 @keyframes fall {
-    100% {
-        transform: translateY(0);
-    }
+  100% {
+    transform: translateY(0);
+  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,74 +1,130 @@
 <template>
-  <div class="home">
-    <div id="name">Applications</div>
-    <div class="parent">
-
-      <router-link class="link" to="/Zneyk" exact>
-        <div class="application">
-          <div class="title">Zneyk</div>
-          <img class="image" src="./../../public/assets/home/zneyk.jpg">
-          <div class="text">Zneyk is a very simple snake game. Try to eat foods, do not hit any blocks which you placed and make a good score.</div>
-        </div>
+  <div class="home-container">
+    <figure class="card">
+      <router-link class="card-link" to="/Zneyk" exact>
+        <img class="card-img" src="./../../public/assets/home/zneyk.jpg" alt="zneyk" />
+        <input class="card-input" type="radio" name="radio-set" checked="checked" />
+        <figcaption class="card-caption">
+            <div class="card-description">
+              <span class="card-title">Zneyk</span>
+              <span>
+                Zneyk is a very simple snake game.
+                Try to eat foods, do not hit any blocks which you placed and make a good score.
+              </span>
+            </div>
+        </figcaption>
       </router-link>
 
-      <router-link class="link" to="/Zhips" exact>
-        <div class="application">
-          <div class="title">Zhips</div>
-          <img class="image" src="./../../public/assets/home/zhips.jpg">
-          <div class="text">Zhips is a battleship game. Place your ships to tiles and attack to enemy tiles.</div>
-        </div>
-      </router-link>
+      <figure class="card">
+        <router-link class="card-link" to="/Zhips" exact>
+          <img class="card-img" src="./../../public/assets/home/zhips.jpg" alt="zneyk" />
+          <input class="card-input" type="radio" name="radio-set" />
+          <figcaption class="card-caption">
+            <div class="card-description">
+              <span class="card-title">Zhips</span>
+              <span>
+                Zhips is a battleship game.
+                Place your ships to tiles and attack to enemy tiles.
+              </span>
+            </div>
+          </figcaption>
+        </router-link>
 
-      <router-link class="link" to="/Aidl" exact>
-        <div class="application">
-          <div class="title">Aidl</div>
-          <img class="image" src="./../../public/assets/home/aidl.jpg">
-          <div class="text">Aidl is an idle rpg game game. Create your character, go to adventures and level up!</div>
-          <div class="additional">(Under construction)</div>
-        </div>
-      </router-link>
+        <figure class="card">
+          <router-link class="card-link" to="/Aidl" exact>
+            <img class="card-img" src="./../../public/assets/home/aidl.jpg" alt="zneyk" />
+            <input class="card-input" type="radio" name="radio-set" />
+            <figcaption class="card-caption">
+              <div class="card-description">
+                <span class="card-title">Aidl</span>
+                <span>
+                  Aidl is an idle rpg game game.
+                  Create your character, go to adventures and level up!
+                </span>
+                <span class="card-additional">(under construction)</span>
+              </div>
+            </figcaption>
+          </router-link>
 
-      <router-link class="link" to="/Morz" exact>
-        <div class="application">
-          <div class="title">Morz</div>
-          <img class="image" src="./../../public/assets/home/morz.jpg">
-          <div class="text">Morz is an application which may help you to learn morse code.</div>
-        </div>
-      </router-link>
+          <figure class="card">
+            <router-link class="card-link" to="/Morz" exact>
+              <img class="card-img" src="./../../public/assets/home/morz.jpg" alt="zneyk" />
+              <input class="card-input" type="radio" name="radio-set" />
+              <figcaption class="card-caption">
+                <div class="card-description">
+                  <span class="card-title">Morz</span>
+                  <span>
+                    Morz is an application which may help you to learn morse code.
+                  </span>
+                </div>
+              </figcaption>
+            </router-link>
 
-      <router-link class="link" to="/Pass" exact>
-        <div class="application">
-          <div class="title">Pass</div>
-          <img class="image" src="./../../public/assets/home/pass.jpg">
-          <div class="text">Pass is a password generator which you can create your own unique password and send encrypted version of your password to your e-mail.</div>
-        </div>
-      </router-link>
+            <figure class="card">
+              <router-link class="card-link" to="/Pass" exact>
+                <img class="card-img" src="./../../public/assets/home/pass.jpg" alt="zneyk" />
+                <input class="card-input" type="radio" name="radio-set" />
+                <figcaption class="card-caption">
+                  <div class="card-description">
+                    <span class="card-title">Pass</span>
+                    <span>
+                      Pass is a password generator which you can create your own unique password and send encrypted version of your password to your e-mail.
+                    </span>
+                  </div>
+                </figcaption>
+              </router-link>
 
-      <router-link class="link" to="/Chat" exact>
-        <div class="application">
-          <div class="title">Chat</div>
-          <img class="image" src="./../../public/assets/home/chat.jpg">
-          <div class="text">Chat application which you can join to a room and chat with your friends.</div>
-        </div>
-      </router-link>
+              <figure class="card">
+                <router-link class="card-link" to="/Chat" exact>
+                  <img class="card-img" src="./../../public/assets/home/chat.jpg" alt="zneyk" />
+                  <input class="card-input" type="radio" name="radio-set" />
+                  <figcaption class="card-caption">
+                    <div class="card-description">
+                      <span class="card-title">Chat</span>
+                      <span>
+                        Chat application which you can join to a room and chat with your friends.
+                      </span>
+                    </div>
+                  </figcaption>
+                </router-link>
 
-      <router-link class="link" to="/Poztman" exact>
-        <div class="application">
-          <div class="title">Poztman</div>
-          <img class="image" src="./../../public/assets/home/poztman.jpg">
-          <div class="text">A postman clone which uses its own api. Try the requests and add your words to the api.</div>
-        </div>
-      </router-link>
+                <figure class="card">
+                  <router-link class="card-link" to="/Poztman" exact>
+                    <img class="card-img" src="./../../public/assets/home/poztman.jpg" alt="zneyk" />
+                    <input class="card-input" type="radio" name="radio-set" />
+                    <figcaption class="card-caption">
+                      <div class="card-description">
+                        <span class="card-title">Poztman</span>
+                        <span>
+                          A postman clone which uses its own api.
+                          Try the requests and add your words to the api.
+                        </span>
+                      </div>
+                    </figcaption>
+                  </router-link>
 
-      <router-link class="link" to="/Vizion" exact>
-        <div class="application">
-          <div class="title">Vizion</div>
-          <img class="image" src="./../../public/assets/home/vizion.jpg">
-          <div class="text">Vizion is an application which contains pose estimation, hand tracking, face detection and face meshing solutions using MediaPipe.</div>
-        </div>
-      </router-link>
+                  <figure class="card">
+                    <router-link class="card-link" to="/Vizion" exact>
+                      <img class="card-img" src="./../../public/assets/home/vizion.jpg" alt="zneyk" />
+                      <input class="card-input" type="radio" name="radio-set" />
+                      <figcaption class="card-caption">
+                        <div class="card-description">
+                          <span class="card-title">Vizion</span>
+                          <span>
+                            Vizion is an application which contains pose estimation, hand tracking, face detection and face meshing solutions using MediaPipe.
+                          </span>
+                        </div>
+                      </figcaption>
+                    </router-link>
 
-    </div>
+                  </figure>
+                </figure>
+              </figure>
+            </figure>
+          </figure>
+        </figure>
+      </figure>
+    </figure>
   </div>
 </template>
 
@@ -81,65 +137,4 @@ export default {
 }
 </script>
 
-<style scoped>
-#name {
-  text-align: center;
-  font-size: 50px;
-}
-
-.parent {
-  display: grid;
-  margin-top: 4px;
-  margin-right: 3em;
-  margin-left: 3em;
-  grid-template-columns: repeat(4, 1fr);
-  grid-template-rows: repeat(2, 1fr);
-  grid-column-gap: 2em;
-  grid-row-gap: 2em;
-}
-
-.application {
-  min-height: 360px;
-  padding-top: 4px;
-  padding-left: 2em;
-  padding-right: 2em;
-  border-radius: 1em;
-  transition: .5s;
-  text-align: center;
-  color: #ffffff;
-}
-.application:hover {
-  border-radius: 3em;
-}
-
-.link {
-  text-decoration: none;
-}
-
-.title {
-  display: block;
-  line-height: 1;
-  padding-bottom: 8px;
-  margin-bottom: 4px;
-  margin-top: 4px;
-  font-size: 40px;
-}
-
-.image {
-  min-height: 200px;
-  max-width: 100%;
-  height: auto;
-}
-
-.text {
-  font-family: 'Noto Sans JP', sans-serif;
-  font-weight: 500;
-  line-height: 1.6;
-  margin-top: 4px;
-}
-
-.additional {
-  line-height: 1.6;
-  font-size: 14px;
-}
-</style>
+<style src='./home.css'></style>
